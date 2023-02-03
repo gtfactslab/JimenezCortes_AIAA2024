7d80de55da1b098a11a5fb8e8b117432955b89ea
carmenjimc <124396898+carmenjimc@users.noreply.github.com>
2023-02-03 15:18:28 -0500

    Delete PTS_SIM_CCTA_EXAMPLE directory

--- PTS_SIM_CCTA_EXAMPLE/EXTRA/runfg.bat
+++
@@ -1,6 +0,0 @@
-SET FG_ROOT=C:\Program Files\FlightGear 2020.3\data 
-.\\bin\fgfs --fg-aircraft=C:\Program Files\FlightGear 2020.3\data\Aircraft\c172p --aircraft=c172p --fdm=null --enable-auto-coordination --native-
-fdm=socket,in,30,localhost,5502,udp --fog-disable --enable-clouds3d --start-date- lat=2004:06:01:09:00:00 --disable-sound --visibility=15000 --in-air --
-enable-terrasync --fg-scenery=C:\Program Files\FlightGear 2020.3\data\gui\dialogs --terrasync-dir=C:\Program Files\FlightGear 2020.3\data\gui\dialogs
-prop:/engines/engine0/running=true --disable-freeze --airport=KMIA --runway=06 --
-altitude=8000 --heading=0 --offset-distance=0 --offset-azimuth=0 --enable-rembrandt
